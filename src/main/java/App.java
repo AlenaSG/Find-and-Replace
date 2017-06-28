33e1e49795c9b3d5a36f039c677fe489fe1053b9
Alena Golovina and Emilie Thoreson <alena@>
2017-06-28 13:10:45 -0700

    add user interface to app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,18 @@
+import java.io.Console;
+
 public class App {
   public static void main(String[] args) {
+    Console myConsole = System.console();
+    System.out.println("Enter a word or phrase");
+    String originalString = myConsole.readLine();
+    System.out.println("Enter a word or phrase you're looking to erase");
+    String doesItContain = myConsole.readLine();
+    System.out.println("What word or phrase would you like to substitue in its place?");
+    String replaceWith = myConsole.readLine();
 
-    System.out.println("I'm a find and replace application!");
-
+    //int intUserNumber = Integer.parseInt(stringUserNumber);
+    FindAndReplace userPhrase = new FindAndReplace();
+    String finalOutput = userPhrase.findAndReplace(originalString, doesItContain, replaceWith);
+    System.out.println(finalOutput);
   }
 }
